98f04b28011de1d9e8ac01e1e40aedc7150cbbc0
Gabe Baduqui <baduquig@gmail.com>
2023-06-24 08:43:32 -0700

    rename project repo and update README description

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# py-zhvi
-Flask  application to serve up Zillow data to personal site Real Estate Analysis project page
+# python-anywhere-projects
+Flask applications to host Python Anywhere personal projects
--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import pandas as pd
 
 app = Flask(__name__)
 
-three_bed = pd.read_csv('https://raw.githubusercontent.com/baduquig/py-zhvi/main/data/3bed.csv')
-four_bed = pd.read_csv('https://raw.githubusercontent.com/baduquig/py-zhvi/main/data/4bed.csv')
-rent = pd.read_csv('https://raw.githubusercontent.com/baduquig/py-zhvi/main/data/rent.csv')
+three_bed = pd.read_csv('https://raw.githubusercontent.com/baduquig/python-anywhere-projects/main/data/3bed.csv')
+four_bed = pd.read_csv('https://raw.githubusercontent.com/baduquig/python-anywhere-projects/main/data/4bed.csv')
+rent = pd.read_csv('https://raw.githubusercontent.com/baduquig/python-anywhere-projects/main/data/rent.csv')
 
 @app.route('/zhvi')
 def filter_data():
