7af463ee2e8335b7785cd66cf5fc5337bd92d9f8
Gabriel Baduqui <baduquig@gmail.com>
2023-08-13 08:24:37 -0700

    create cfb_pickem endpoints

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from flask import Flask, jsonify, request
-import json
-import mysql.connector
 import pandas as pd
+
+from cfb_pickem.all_data import AllData
+from cfb_pickem.pick import SubmitPicks
+from cfb_pickem.register import Register
+from cfb_pickem.validate import Validate
 
 app = Flask(__name__)
 
@@ -81,7 +84,33 @@ def return_data():
 #####################################
 # CFB Pickem Endpoints              #
 #####################################
+@app.route('/validate')
+def validate():
+    username = request.args.get('username')
+    pw = request.args.get('pw')
 
+    response = Validate.validate(username, pw)
+    return response
+
+@app.route('/register')
+def register():
+    username = request.args.get('username')
+    pw = request.args.get('pw')
+    confirm_pw = request.args.get('confirm_pw')
+
+    response = Register.register(username, pw, confirm_pw)
+    return response
+
+@app.route('/alldata')
+def all_data():
+    response = AllData.get_all_data()
+    return response
+
+@app.route('/submitpick')
+def submit_pick():
+    data = request.json
+    response = SubmitPicks.submit_picks(data)
+    return response
 #####################################
 #####################################
 
